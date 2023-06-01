f351ff3df51b81fa994204c95c0b125bf6fcf8ce
armin <arminjamali73@gmail.com>
2023-06-01 14:38:32 +0330

    created constructor and implemented set price method

--- src/main/java/Fiction.java
+++ src/main/java/Fiction.java
@@ -1,8 +1,12 @@
 public class Fiction extends Book {
 
+    public Fiction(String title) {
+        super(title);
+        setPrice();
+    }
 
     @Override
     public void setPrice() {
-
+        this.price=24.99;
     }
 }
