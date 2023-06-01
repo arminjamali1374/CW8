c25763a91fc01434b1b0e5b01acdf540e389e224
armin <arminjamali73@gmail.com>
2023-06-01 14:56:23 +0330

    insert 10 values to books array

--- src/main/java/BookArray.java
+++ src/main/java/BookArray.java
@@ -1,2 +1,19 @@
 public class BookArray {
+
+
+    public static void main(String[] args) {
+        Book book1=new Fiction("first");
+        Book book2= new Fiction("SECOND");
+        Book book3= new Fiction("three");
+        Book book4= new Fiction("four");
+        Book book5= new Fiction("five");
+        Book book6= new NonFiction("SIX");
+        Book book7= new NonFiction("SEVEN");
+        Book book8= new NonFiction("EIGHT");
+        Book book9= new NonFiction("NINE");
+        Book book10= new NonFiction("THEN");
+
+        Book []books= {book1,book2,book3,book4,book5,book6,book7,book8,book9,book10};
+    }
+
 }
